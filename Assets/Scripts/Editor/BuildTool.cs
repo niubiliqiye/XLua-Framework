aa6c20c48352a79edd3dcfd0d3a972077460f5c9
niubi_liqiye <12200904+niubi-liqiye@user.noreply.gitee.com>
2023-03-31 21:11:50 +0800

    完善BuildTool

--- Assets/Scripts/Editor/BuildTool.cs
+++ Assets/Scripts/Editor/BuildTool.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -28,6 +29,9 @@ public class BuildTool : Editor
     {
         List<AssetBundleBuild> assetBundleBuilds = new List<AssetBundleBuild>();
 
+        //文件信息列表
+        List<string> bundleInfos = new List<string>();
+
         string[] files = Directory.GetFiles(PathUtil.BuildResourcesPath, "*", SearchOption.AllDirectories);
 
         for (int i = 0; i < files.Length; i++)
@@ -40,7 +44,7 @@ public class BuildTool : Editor
             AssetBundleBuild assetBundle = new AssetBundleBuild();
 
             string fileName = PathUtil.GetStanderPath(files[i]);
-            Debug.Log("file:"+fileName);
+            Debug.Log("file:" + fileName);
             
             string assetName = PathUtil.GetUnityPath(fileName);
             assetBundle.assetNames = new string[] { assetName };
@@ -48,8 +52,19 @@ public class BuildTool : Editor
             string bundleName = files[i].Replace(PathUtil.BuildResourcesPath, "").ToLower();
             assetBundle.assetBundleName = bundleName + ".ab";
             assetBundleBuilds.Add(assetBundle);
-        }
 
+            //添加文件和依赖信息
+            List<string> dependenceInfo = GetDependence(assetName);
+            string bundleInfo = assetName + "|" + bundleName;
+
+            if (dependenceInfo.Count>0)
+            {
+                bundleInfo = bundleInfo + "|" + string.Join("|", dependenceInfo);
+            }
+            
+            bundleInfos.Add(bundleInfo);
+        }
+        
         if (Directory.Exists(PathUtil.BundleOutpath))
         {
             Directory.Delete(PathUtil.BundleOutpath, true);
@@ -59,5 +74,22 @@ public class BuildTool : Editor
         
         BuildPipeline.BuildAssetBundles(PathUtil.BundleOutpath, assetBundleBuilds.ToArray(),
             BuildAssetBundleOptions.None, target);
+
+        File.WriteAllLines(PathUtil.BundleOutpath + "/" + AppConst.FileListName, bundleInfos);
+        
+        AssetDatabase.Refresh();
+    }
+
+    /// <summary>
+    /// 获取依赖文件列表
+    /// </summary>
+    /// <param name="curFile"></param>
+    /// <returns></returns>
+    static List<string> GetDependence(string curFile)
+    {
+        List<string> dependence = new List<string>();
+        string[] files = AssetDatabase.GetDependencies(curFile);
+        dependence = files.Where(file => !file.EndsWith(".cs") && !file.Equals(curFile)).ToList();
+        return dependence;
     }
 }
